c39f0d26e9300352f525bf454c82441bee344e0f
Jimmy <1064425721@qq.com>
2022-08-30 21:13:36 +0800

    fix: 增加部分非空判断

--- src/components/dragable/useDragable.ts
+++ src/components/dragable/useDragable.ts
@@ -1,52 +1,68 @@
 
 import { Ref, ref, watch } from "vue";
-import { Direction, Position, Target } from "./type"
+
+interface Position {
+    x: number
+    y: number
+}
+
+type Direction = 'right' | 'left' | 'top' | 'bottom'
+
+type Target = Ref<HTMLElement | null>
 /**
   * @description 拖拽组件
   * @param {HTMLElement} target 鼠标初始按下位置
   * @param {'right' | 'left' | 'top' | 'bottom'}  direction 拖拽方向
   * @param {number} triggerBoundary 拖拽边界(默认为2,作为盒子的百分比分母)
-  * @param {() => void} triggerFn 拖拽触发成功回调
-  * @param {() => void} triggeredFn 拖拽触发成功完成后的回调
+  * @param {(target: Target) => void} triggerFn 拖拽触发成功回调
+  * @param {(target: Target) => void} triggeredFn 拖拽触发成功完成后的回调
   */
 
 export const useDragable = (
     target: Target,
     direction: Direction,
+    triggerBoundary: number,
     triggerFn: (target: Target) => void,
     triggeredFn: (target: Target) => void,
-    triggerBoundary: number) => {
+) => {
     /**
      * @description 鼠标初始按下位置[常量]
      */
-
     const initPosition = ref<Position>()
+
     /**
      * @description 鼠标移动点击的位置
      */
     const movePosition = ref<Position>()
+
     /**
      * @description target盒子的宽、高[常量]
      */
     const tagrgetDesc = ref<Position>()
+
     /**
      * @description 移动的距离
      */
     const pressedDelta = ref<Position>()
+
     /**
     * @description 是否被拖拽
     */
     const isDrag = ref<boolean>(false)
+
     /**
      * @description 是否成功拖拽
      */
     const isSuccess = ref<boolean>(false)
+
     /**
      * @description 记录目标的style
      */
-    const elStyle = window.getComputedStyle(target.value).backgroundColor
-
-
+    let elStyle: string
+    
+    if (target.value) {
+        elStyle = window.getComputedStyle(target.value).backgroundColor
+    }
     const start = (e: MouseEvent) => {
         isDrag.value = true
         initPosition.value = {
@@ -138,10 +154,8 @@ export const useDragable = (
     target.value?.addEventListener("mousemove", move);
     target.value?.addEventListener("mouseup", up);
     target.value?.addEventListener("mouseout", out);
-    watch(isSuccess, (val) => {
-        console.log(val)
-        console.log(elStyle)
-        val ? triggerFn(target) : target.value.style.backgroundColor = elStyle
+    watch(isSuccess, (val: boolean) => {
+        val ? triggerFn(target) : target.value!.style.backgroundColor = elStyle
 
     })
 }
